dbd49a08b7aac465b5e7ca08e6a5f10da1002804
KevinLu <kevinlu0102@gmail.com>
2024-03-31 13:22:12 +0800

    Add Constants struct for managing identifiers

---
+++ Pokedex/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Pokedex
+//
+//  Created by Kevin Lu on 2024/3/31.
+//
+
+import Foundation
--- Pokedex/Model/Customs/PokemonFavorite.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PokemonFavorite.swift
-//  Pokedex
-//
-//  Created by Kevin Lu on 2024/3/26.
-//
-
-import Foundation
